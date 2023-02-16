b1ef0bf24a44cb4e3c8f26ec5957824aab60fc8b
andyneilson42 <andy.neilson.42@gmail.com>
2023-02-16 15:23:06 -0500

    Comments

--- src/main/scala/andy42/graph/model/EventHasEffectOps.scala
+++ src/main/scala/andy42/graph/model/EventHasEffectOps.scala
@@ -4,10 +4,20 @@ import zio.Task
 
 object EventHasEffectOps:
 
+  /**
+    * Remove any events that would have no effect on the given snapshot.
+    * This is calculated in a way that does not copy `events` if there is no
+    * change to the state. This avoids heap allocation pressure for this frequently
+    * executed operation.
+    */
   def filterHasEffect(events: Vector[Event], nodeSnapshot: NodeSnapshot): Vector[Event] =
     if !events.exists(hasEffect(_, nodeSnapshot)) then events
     else events.filter(hasEffect(_, nodeSnapshot))
 
+  /**
+    * Determine if an Event would have any effect if applied to a Node's history
+    * given the collapesed node snapshot calculated for that point in time.
+    */  
   def hasEffect(event: Event, nodeSnapshot: NodeSnapshot): Boolean =
     event match
       case Event.NodeRemoved =>
