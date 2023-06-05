5c1d92cbc7aaea8954a091d22b15ace2c4f7aa75
andyneilson42 <andy.neilson.42@gmail.com>
2023-06-05 14:32:29 -0400

    Separate out EdgeReconciliationRepository trait from the Postgres implementation.

--- src/main/scala/andy42/graph/services/EdgeReconciliationRepository.scala
+++ src/main/scala/andy42/graph/services/EdgeReconciliationRepository.scala
@@ -1,8 +1,6 @@
 package andy42.graph.services
 
-import andy42.graph.model.{Edge, NodeId}
-import io.getquill.*
-import io.getquill.context.qzio.ImplicitSyntax.*
+import andy42.graph.model.*
 import zio.*
 
 import javax.sql.DataSource
@@ -34,39 +32,3 @@ object EdgeReconciliationSnapshot:
     */
   def unknown(windowStart: Long, windowSize: Long): EdgeReconciliationSnapshot =
     EdgeReconciliationSnapshot(windowStart, windowSize, Unknown)
-
-final case class EdgeReconciliationRepositoryLive(ds: DataSource) extends EdgeReconciliationRepository:
-
-  val ctx: PostgresZioJdbcContext[Literal] = PostgresZioJdbcContext(Literal)
-  import ctx.*
-
-  private inline def edgeReconciliationTable: Quoted[EntityQuery[EdgeReconciliationSnapshot]] = quote {
-    query[EdgeReconciliationSnapshot]
-  }
-
-  private inline def quotedMarkWindow(
-      edgeReconciliation: EdgeReconciliationSnapshot
-  ): Quoted[Insert[EdgeReconciliationSnapshot]] = quote {
-    edgeReconciliationTable
-      .insertValue(lift(edgeReconciliation))
-      .onConflictUpdate(_.windowStart)(
-        (table, excluded) => table.windowSize -> excluded.windowSize,
-        (table, excluded) => table.state -> excluded.state
-      )
-  }
-
-  given Implicit[DataSource] = Implicit(ds)
-
-  override def markWindow(edgeReconciliation: EdgeReconciliationSnapshot): UIO[Unit] =
-    run(quotedMarkWindow(edgeReconciliation)).implicitly
-      .retry(Schedule.recurs(5)) // TODO: Configure retry policy - either exponential or fibonacci
-      // TODO: Check that exactly one row is changed, otherwise log error
-      // TODO: Log if retries fails
-      .foldZIO(_ => ZIO.unit, _ => ZIO.unit) // TODO: Is this the best way to consume errors
-
-object EdgeReconciliationRepository:
-  val layer: URLayer[DataSource, EdgeReconciliationRepository] =
-    ZLayer {
-      for ds <- ZIO.service[DataSource]
-      yield EdgeReconciliationRepositoryLive(ds)
-    }
---
+++ src/main/scala/andy42/graph/services/PostgresEdgeReconciliationRepository.scala
@@ -0,0 +1,43 @@
+package andy42.graph.services
+
+import io.getquill.*
+import io.getquill.context.qzio.ImplicitSyntax.*
+import zio.*
+
+import javax.sql.DataSource
+
+final case class PostgresEdgeReconciliationRepositoryLive(ds: DataSource) extends EdgeReconciliationRepository:
+
+  val ctx: PostgresZioJdbcContext[Literal] = PostgresZioJdbcContext(Literal)
+  import ctx.*
+
+  private inline def edgeReconciliationTable: Quoted[EntityQuery[EdgeReconciliationSnapshot]] = quote {
+    query[EdgeReconciliationSnapshot]
+  }
+
+  private inline def quotedMarkWindow(
+      edgeReconciliation: EdgeReconciliationSnapshot
+  ): Quoted[Insert[EdgeReconciliationSnapshot]] = quote {
+    edgeReconciliationTable
+      .insertValue(lift(edgeReconciliation))
+      .onConflictUpdate(_.windowStart)(
+        (table, excluded) => table.windowSize -> excluded.windowSize,
+        (table, excluded) => table.state -> excluded.state
+      )
+  }
+
+  given Implicit[DataSource] = Implicit(ds)
+
+  override def markWindow(edgeReconciliation: EdgeReconciliationSnapshot): UIO[Unit] =
+    run(quotedMarkWindow(edgeReconciliation)).implicitly
+      .retry(Schedule.recurs(5)) // TODO: Configure retry policy - either exponential or fibonacci
+      // TODO: Check that exactly one row is changed, otherwise log error
+      // TODO: Log if retries fails
+      .foldZIO(_ => ZIO.unit, _ => ZIO.unit) // TODO: Is this the best way to consume errors
+
+object PostgresEdgeReconciliationRepository:
+  val layer: URLayer[DataSource, EdgeReconciliationRepository] =
+    ZLayer {
+      for ds <- ZIO.service[DataSource]
+      yield PostgresEdgeReconciliationRepositoryLive(ds)
+    }
